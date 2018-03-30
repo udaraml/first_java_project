1e6fc3a10c200f323a167ab164b951c485fa70e8
udaraml <gamersum.50@gmail.com>
2018-03-30 12:54:09 +0530

    commit

--- Test/src/main/java/TestApp/Calculator.java
+++ Test/src/main/java/TestApp/Calculator.java
@@ -4,22 +4,22 @@ package TestApp;
  * Created by udara on 3/28/18.
  */
 public class Calculator {
-    private double answer;
+    private double answer; 
 
     public double calculateValue(char operator, double operand1, double operand2){
 
     switch(operator){
         case '+':
-            answer = operand1 + operand2;
+            answer = operand1 + operand2; //addition
             break;
         case '-':
-            answer = operand1 - operand2;
+            answer = operand1 - operand2; //substraction
             break;
         case '*':
-            answer = operand1 * operand2;
+            answer = operand1 * operand2; //multiplication
             break;
         case '/':
-            answer = operand1 / operand2;
+            answer = operand1 / operand2; //division
             break;
     }
     return answer;
